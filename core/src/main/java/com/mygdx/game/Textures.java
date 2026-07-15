d4278bc1b4a210709834daf48fa387bead139813
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-07-15 19:15:07 +0300

    changed the placeholder texture

--- core/src/main/java/com/mygdx/game/Textures.java
+++ core/src/main/java/com/mygdx/game/Textures.java
@@ -8,7 +8,7 @@ public class Textures {
 	public final static Texture backGround = new Texture("main_game_bg.jpeg");
 	public final static Texture openedSpringTrap =  new Texture("spring_trap_opened.png");
 	public final static Texture closedSpringTrap = new Texture("spring_trap_closed.png");
-	public final static Texture PlaceHolder = new Texture("PlaceHolder.png");
+	public final static Texture PlaceHolder = new Texture("place_holder.png");
 	
 	
 	public static void dispose() {
