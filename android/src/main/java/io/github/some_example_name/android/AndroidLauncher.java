1aa7de4ad7772c7626a85464527af71d4701eb70
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-07-18 20:39:59 +0300

    now can launch on android!

--- android/src/main/java/io/github/some_example_name/android/AndroidLauncher.java
+++ android/src/main/java/io/github/some_example_name/android/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import io.github.some_example_name.Main;
+import com.mygdx.game.Main;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
